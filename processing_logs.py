432a2969c6fbae6528bfa467f459b0f46a2c9224
Veselka Marinova <vsl.marinova@gmail.com>
2025-03-13 17:52:26 +0200

    T1086: Start on log processing script.

---
+++ processing_logs.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+
+load_dotenv()
+# Read values from environment variables
+USERNAME  = os.getenv("SCRAPYD_USERNAME")
+PASSWORD = os.getenv("SCRAPYD_PASSWORD")
+BASE_URL = os.getenv("SCRAPYD_URL")
+
+
+basic = HTTPBasicAuth(USERNAME, PASSWORD)
+url = BASE_URL + "/listjobs.json"
+response = requests.get(url, auth=basic)
+
+
