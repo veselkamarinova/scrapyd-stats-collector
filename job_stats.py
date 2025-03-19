e2f774c42e3b060c2cb2a240379ec907e12a3b3c
Veselka Marinova <vsl.marinova@gmail.com>
2025-03-19 09:57:35 +0200

    T1086: Refactor create_from_logfile method to ensure integer values are extracted correctly from resuolt_dict and handle missing keys appropriately.

--- job_stats.py
+++ job_stats.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import json
+import datetime
 
 
 class JobStats:
@@ -34,10 +35,52 @@ class JobStats:
         # Example regex to extract some information from the log content
         regex_pattern = r"\[scrapy\.statscollectors].*\n({.*})"
         matches = re.findall(regex_pattern, log_content, re.DOTALL | re.MULTILINE)
-        # Example JSON processing
-        json_data = json.loads(matches[0])
-        # Process the extracted data as needed
-        print(f"Log content for job {id}: {log_content}")
+        # Remove curly braces and match key-value pairs
+        dict_string = matches[0].strip("{}")
+        pattern = re.compile(
+            r"('.*?': .*?datetime\.datetime\(\d{4}, \d{1,2}, \d{1,2}, \d{1,2}, \d{1,2}, \d{1,2}, \d{1,6}\)|'.*?': '.*?'|'.*?': \d+|'.*?': \d+\.\d+)"
+        )
+        matchess = pattern.findall(dict_string)
+
+        # Initialize an empty dictionary
+        result_dict = {}
+
+        # Process each key-value pair
+        for match in matchess:
+            key, value = match.split(": ", 1)
+            # Convert key to string and value to appropriate type
+            key = key.strip("'")
+            if "datetime.datetime" in value:
+                value = eval(value)
+            elif value.isdigit():
+                value = int(value)
+            elif value.replace(".", "", 1).isdigit():
+                value = float(value)
+            else:
+                value = value.strip("'")
+            result_dict[key] = value
+
+        scraped_count = (
+            result_dict["item_scraped_count"]
+            if "item_scraped_count" in result_dict
+            else 0
+        )
+        dropped_count = (
+            result_dict["item_dropped_count"]
+            if "item_dropped_count" in result_dict
+            else 0
+        )
+        warning_count = (
+            result_dict["log_count/WARNING"]
+            if "log_count/WARNING" in result_dict
+            else 0
+        )
+        error_count = (
+            result_dict["log_count/ERROR"] if "log_count/ERROR" in result_dict else 0
+        )
+        max_mem = result_dict["memusage/max"]
+        status = result_dict["finish_reason"]
+
         return JobStats(
             job_id,
             spider,
